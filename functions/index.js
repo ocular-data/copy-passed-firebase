cae0f766878b36cc4964949e1d76b5c9236e8b4c
zivoy <ziv132@gmail.com>
2020-04-22 14:54:57 -0400

    push history up by one for every new update

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const max_history = 9;
+
+exports.pushHistory = functions.database.ref('/users/{Uid}')
+    .onWrite((change, context) => {
+        if (!change.before.exists()) {
+            return null;
+        }
+        if (!change.after.exists()) {
+            return null;
+        }
+        if (change.before.child("last").val()===change.after.child("last").val()){
+            return null
+        }
+
+        let before = change.before.val()
+        let after = change.after.val()
+
+        after["1"] = before["last"]
+
+        for (let i=1; i<max_history; i++){
+            if (!(i.toString() in before)) break;
+            after[(i+1).toString()] = before[i.toString()]
+            change.before.ref
+        }
+
+        return change.after.ref.set(after);
+    });
